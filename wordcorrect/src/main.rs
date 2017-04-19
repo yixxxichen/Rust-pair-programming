be327bd507bbedd5f07c5e834e00048d3c242d98
Yixi Chen <ecchen96153@gmail.com>
2017-04-18 23:40:13 -0500

    change the function in main

--- wordcorrect/src/main.rs
+++ wordcorrect/src/main.rs
@@ -4,6 +4,82 @@ use std::collections::HashSet;
 fn main() {
     let mut possible_words: HashSet<String> = HashSet::new();
 }
+fn find(trie: & Trie, path: & mut String,pathclone: & mut String,cur: & mut String, op: usize)-> Option<Result>{
+	if pathclone.len()==0 && trie.value>0{
+		return Result{
+			value: trie.value,
+			key: cur.clone(),
+		}
+	else{
+		let mut temppath = pathclone.clone();
+        let mut currchar = temppath.remove(0);
+        cur.push(currchar);
+        let mut max = Result::new();
+        let mut currtrie = trie.children.get(&curchar).unwrap();
+        if currtrie {
+        	max=find(currtrie,path,& mut temppath, cur, op);
+        }
+        else if op>0{
+        	let mut temp = Result::new();
+        	//insertion
+        	for key in trie.children.keys(){
+        		cur.push(*key);
+        		currtrie = trie.children.get(&key).unwrap();
+        		temp = find(currtrie,path,pathclone,cur,op-1);
+        		if temp.value>max.value{
+        			max = temp;
+        		}
+        	}
+        	//deletion
+        	//if we can get a word after deleting current character
+        	if pathclone.len()==1 && trie.value>0{
+        		return Result{
+        			value: trie.value,
+        			key: cur.clone(),
+        		}
+        	else{
+        		temppath = pathclone.clone();
+        		temppath.remove(0);
+        		curchar = pathclone.remove(0);
+        		cur.push(curchar);
+        		currtrie = trie.children.get(&curchar).unwrap();
+        		if currtrie{
+        			temp = find(currtrie,path,pathclone,cur,op-1);
+        			if temp.value>max.value{
+        				max = temp;
+        			}
+        		}
+        	//transpose
+        	if path.len()>1{
+        		temppath.pathclone.clone();
+        		curchar = temppath.remove(0);
+        		temppath.insert(1,curchar);
+        		curchar = temppath.remove(0);
+        		cur.push(curchar);
+        		currtrie = trie.children.get(&curchar).unwrap();
+        		if currtrie {
+        			temp = find(currtrie,path,pathclone,cur,op-1);
+        			if temp.value>max.value{
+        				max = temp;
+        			}
+        		}
+        		
+        	}
+        	}
+        	}
+        }
+	}
+	}
+}
 mod readinput;
 mod edit;
 mod trie;
+#[test]
+fn test_find_edit(){
+	use super::{trie,Result};
+    let mut t = Trie::new();
+    t.insert(&mut "ac".to_string(), 4);
+    t.insert(&mut "bc".to_string(), 5);
+    assert_eq!(find(&mut "bc".to_string(),&mut "bc".to_string(),&mut "".to_string(),2).value, 5);
+    assert_eq!(find(&mut "ac".to_string(),&mut "ac".to_string(),&mut "".to_string(),2).value, 4);
+}
--- wordcorrect/src/trie.rs
+++ wordcorrect/src/trie.rs
@@ -2,7 +2,7 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::string::String;
-use std::clone;
+use std::clone::Clone;
 
 struct Trie {
     value: usize,
@@ -11,6 +11,24 @@ struct Trie {
 struct Result{
     pub value: usize,
     pub key: String,
+}
+impl Result{
+    fn new() -> Result {
+        Result {
+            value: 0,
+            key: String::new(),
+        }
+    }
+}
+impl Clone for Result{
+    fn clone(&self) ->Self{
+        let mut result =Result::new();
+        {
+            result.value = self.value;
+            result.key = self.key.clone();
+        }
+        result
+    }
 }
 impl Trie{
     fn new() -> Trie {
@@ -39,74 +57,75 @@ impl Trie{
                     .fetch(path)
         }
     }
-    fn find_edit(&self, path: &mut String,cur: &mut String, op: usize)-> Option<&mut Result>{
+    fn find_edit(&self, path: & mut String,pathclone: & mut String,cur: & mut String, op: usize)-> Option<Result>{
         match path.len() {
-            0 => Some(&mut Result{
+            0 =>Some(Result{
                         value :self.value,
-                        key : *cur,
-                        }),
-            _ => Some({
-                match self.children.get(&path.clone().chars().next().unwrap()) {
-                    Some(trie) => Some({
-                        let curchar = path.remove(0);
+                        key : cur.clone(),
+                        }
+                    ),
+            _ => {
+                        let mut temppath = pathclone.clone();
+                        let mut curchar = temppath.remove(0);
                         cur.push(curchar);
-                     return   self.children.get(&curchar).unwrap()
-                    .find_edit(path,cur,op);}),
-                    None => Some(if op>0{
-                        let mut max = Result
-                        {
-                            value:0,
-                            key:"".to_string(),
-                        };
-                        let mut temp = Result{
-                            value:0,
-                            key:"".to_string(),
-                        };
+                     let mut max = self.children.get(&curchar).unwrap().find_edit(path,& mut temppath,cur,op).unwrap();
+                        if op>0 {
+                        let mut temp = Result::new();
                         //insertion 
                             for key in self.children.keys(){
                                 cur.push(*key);
-                                temp = *self.children.get(&key)
+                                temp = self.children.get(&key)
                                 .unwrap()
-                                .find_edit(path,cur,op-1).unwrap();
+                                .find_edit(path,pathclone,cur,op-1).unwrap().clone();
                                 if temp.value > max.value{
                                     max=temp;
+
                                     }
                             };
                             //deletion
-                            let pathclone = path.clone();
-                            pathclone.remove(0);
-                            let curchar = pathclone.remove(0);
+                            if path.len()>=1{
+                            temppath = pathclone.clone();
+                            temppath.remove(0);
+                            curchar = pathclone.remove(0);
                             cur.push(curchar);
-                            let mut temp = *self.children.get(&curchar).unwrap().find_edit(path,cur,op-1).unwrap();
+                            temp = self.children.get(&curchar).unwrap().find_edit(path,& mut temppath,cur,op-1).unwrap().clone();
                             if temp.value > max.value{
                                     max=temp;
 
                                     }
+                                }
                             //transpose
-                            pathclone = path.clone();
-                            curchar = pathclone.remove(0);
-                            pathclone.insert(1,curchar);
-                            curchar = pathclone.remove(0);
+                            if path.len()>=2{
+                            temppath = pathclone.clone();
+                            curchar = temppath.remove(0);
+                            temppath.insert(1,curchar);
+                            curchar = temppath.remove(0);
                             cur.push(curchar);
-                            temp = *self.children.get(&curchar).unwrap().find_edit(path,cur,op-1).unwrap();
+                            temp = self.children.get(&curchar).unwrap().find_edit(path,& mut temppath,cur,op-1).unwrap().clone();
                             if temp.value > max.value{
                                     max=temp;
                                     }
+                                }
                             //replace
+                            if path.len()>0{
                             for key in self.children.keys(){
-                                path.remove(0);
+                                temppath = pathclone.clone();
+                                temppath.remove(0);
                                 cur.push(*key);
-                                temp = *self.children.get(&key)
+                                temp = self.children.get(&key)
                                 .unwrap()
-                                .find_edit(path,cur,op-1).unwrap();
+                                .find_edit(path,& mut temppath,cur,op-1).unwrap().clone();
                                 if temp.value > max.value{
                                     max=temp;
                                     }
-                                    return Some(&mut max);
+                                }
                             }
-                        })
-                }
-            })
+                                    
+                            }
+                            return Some(max);
+                
+
+            }
         }
     }
 }
@@ -136,4 +155,12 @@ fn insert_works_if_none() {
     assert_eq!(t.fetch(&mut "a".to_string()), 0);
     assert_eq!(t.fetch(&mut "bc".to_string()), 5);
     assert_eq!(t.fetch(&mut "ac".to_string()), 4);
+}
+#[test]
+fn test_find_edit(){
+    let mut t = Trie::new();
+    t.insert(&mut "ac".to_string(), 4);
+    t.insert(&mut "bc".to_string(), 5);
+    assert_eq!(t.find_edit(&mut "bc".to_string(),&mut "bc".to_string(),&mut "".to_string(),2).unwrap().value, 5);
+    assert_eq!(t.find_edit(&mut "ac".to_string(),&mut "ac".to_string(),&mut "".to_string(),2).unwrap().value, 4);
 }
