1ea9a32717a383f3ca1ef40fb862c5dca0c34810
GanZ2016 <ganzhang@u.northwestern.edu>
2017-04-24 20:59:25 -0500

    bug

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -15,24 +15,26 @@ fn main() {
     //let mut graph = Graph::new()
     
 }
-
 pub fn read_input<R: Read>(reader: R) -> Graph {
     //let mut input: Vec<String> = Vec::new();  
     let mut graph = Graph::new();
-
+    let mut hash:HashMap<String,Vec<&str>> = HashMap::new();
     let mut lines = BufReader::new(reader).lines();
     let mut counter = 0;
     while let Some(Ok(line)) = lines.next() {
         if line == "999" {break}
             let temp = line.clone();
-            
-            let mut vertices: Vec<&str> = temp.split_whitespace().collect();            
+            let mut vertices: Vec<&str> = temp.split_whitespace().collect();
             if vertices.is_empty() {
                 println!("Found empty line");
             }
             else {
                 graph.set_index(&vertices,counter);
-            }    
+                // let other_nodes = &vertices[1..].to_vec();
+                // let node = &vertices[0].to_string();
+                // hash.insert(node.clone(),other_nodes.clone());
+                hash.insert(vertices[0].to_string(),vertices[..].to_vec().clone());
+            }
     }
     return graph;
 }
