27fc90709949d34bc47801e135aada826682a5ad
GanZ2016 <ganzhang@u.northwestern.edu>
2017-04-25 21:22:57 -0500

    comment

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -1,4 +1,34 @@
-// find path
+find path
+
+Read a graph from graph.dat. And select a start point and an end point,
+This program will find the shortest path and print out. If no path is found,
+it will print a message.
+
+Each line is a list of word, where the first word names some node 
+in the graph and the remaining words enumerate its neighbors.
+
+
+
+The user enters a start node and an end node on stdin, one at a time. 
+A query consists of two node names, a starting node and an ending node. 
+The program then prints out a path between the nodes, or a message - "No path found".
+
+INPUT & OUTPUT:
+$ cat graph.dat
+    a b b d
+    b d
+    c d
+    d
+
+Each node must take one line, no empty line is allowed in graph.dat, and 
+duplicated neighbors in one line are allowed.
+Every node mentioned as a neighbor must start a line as well, 
+and no node may start more than one line. Otherwise it will panic with "wrong number of nodes".
+
+$ cargo run graph.dat
+->  a d
+    a d
+->  
 
 
 
@@ -47,11 +77,13 @@ pub fn read_input<R: Read>(reader: R) -> Graph {
 pub fn find_path<R: Read, W:Write>(reader: R, writer: &mut W, graph: Graph )  {
     let mut lines = BufReader::new(reader).lines();
     while let Some(Ok(line)) = lines.next() {
+        if line == "999" {break}
         let input_nodes: Vec<&str> = line.split_whitespace().collect();
         if input_nodes.len() != 2 {
             panic!("wrong start or end node");
         }
         else {
+            
             let res = bfs::bfs(&graph.map,input_nodes[0].to_string(),input_nodes[1].to_string());
             if res.len() == 0 {
                 write!(writer, "No path found.\n").unwrap();
@@ -63,7 +95,7 @@ pub fn find_path<R: Read, W:Write>(reader: R, writer: &mut W, graph: Graph )  {
                 write!(writer, "\n").unwrap();
             }
         }
-        break
+        
     }
 
 }
