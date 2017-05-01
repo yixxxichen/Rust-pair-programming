1d2de61dcac7004df26b7e240e4e292c316e1287
GanZ2016 <ganzhang@u.northwestern.edu>
2017-04-30 20:28:33 -0500

    add write response

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -12,12 +12,12 @@ fn main() {
     // and gets it ready to accept connections.
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let stream = listener.accept().unwrap().0;
-    handle_request(stream);
+    //handle_request(stream);
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 thread::spawn(move || {
-                        handle_request(stream);
+                        get_response(&mut stream);
 
                     });
                 println!("success")
@@ -28,9 +28,15 @@ fn main() {
 
 }
 
-fn handle_request(stream: TcpStream)  {
-    let request = request::handle_stream(stream);
-    for i in 0..3{
-        println!("{}",request[i]);
+fn get_response(stream: &mut TcpStream)  {
+    let req = request::handle_stream(stream);
+    match request::check_request(&req) {
+        Ok(_) => {
+            let mut create_req = request::create_response(&req);
+            let write_res = create_req.write_response();
+            &stream.write(write_res.as_bytes());          
+        }
+        Err(e) => {} 
+        //&stream.write(e.to_string().as_bytes().unwrap())
     }
 }
--- web_server/src/request.rs
+++ web_server/src/request.rs
@@ -6,12 +6,36 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 pub struct Response {
-	status_info: String,
+	protocal: String,
 	status_code: String,
 	server_name: String,
 	file_type: String,
 	file_length: usize,
 	file_content: String,
+}
+
+impl Response {
+    pub fn write_response(&mut self) -> String {
+        // let pro = self.protocal.to_string();
+        // let sta = self.status_code.to_string();
+        // let ser = self.server_name.to_string();
+        // let typ = self.file_type.to_string();
+        // let leng = self.file_length;
+        // let cont = self.file_content.to_string();
+        let mut res = String::new();
+        let leng = self.file_length.clone().to_string();
+        res.push_str(&self.protocal);
+        res.push_str(&self.status_code);
+        res.push_str(&self.server_name);
+        res.push_str(&self.file_type);
+        res.push_str(&leng);
+        res.push_str(&self.file_content);
+        //res = self.protocal + &self.status_code.clone() +&self.server_name.clone() + &self.file_type.clone() + &leng.clone() + &self.file_content.clone();
+        return res;
+        //let stream = res.as_bytes();
+             
+    }
+    
 }
 
 pub enum Error {
@@ -28,7 +52,7 @@ pub enum Good {
 }
 
 //Get stream content and return in Vec<String>
-pub fn handle_stream(stream: TcpStream) -> Vec<String> {
+pub fn handle_stream(stream: &mut TcpStream) -> Vec<String> {
     let mut reader = BufReader::new(stream).lines();
     let mut input = String::new();
     let mut res: Vec<String> = Vec::new();
@@ -47,7 +71,7 @@ pub fn handle_stream(stream: TcpStream) -> Vec<String> {
 // • 400 Bad Request, which indicates that the command is not a properly formatted GET command;
 // • 403 Forbidden, which rejects a command because it specifies a file that is off-limits; and
 // • 404 Not Found, which informs the client that the specified file does not exist.
-pub fn check_request(req: &Vec<&String>) -> Result<Good,Error> {
+pub fn check_request(req: &Vec<String>) -> Result<Good,Error> {
     //check request format: - ERR400
     //GET
     //PATH
@@ -55,13 +79,13 @@ pub fn check_request(req: &Vec<&String>) -> Result<Good,Error> {
     if req.len() < 3 {
         return Err(Error::ERROR400);
     }
-    let check_GET = req[0];
-    let get_path = req[1];
-    let protocol  = req[2];
+    let check_GET = &req[0];
+    let get_path = &req[1];
+    let protocol  = &req[2];
     if check_GET == "GET" && protocol.contains("HTTP") {
         //check file exists - ERR404
         //let path_string = file_path;
-        let file_path = Path::new(get_path);
+        let file_path = Path::new(&get_path);
         if file_path.exists() {
             //let get_file = File::open(&file_path);
             match File::open(&file_path) {
@@ -77,6 +101,34 @@ pub fn check_request(req: &Vec<&String>) -> Result<Good,Error> {
         return Err(Error::ERROR400);
     }
 }
+
+pub fn create_response(req: &Vec<String>) -> Response{
+    let check_GET = &req[0];
+    let get_path = &req[1];
+    let protocal_info  = &req[2];
+    let file_path = Path::new(&get_path);
+    let mut buf = String::new();
+    let mut f = File::open(&file_path).unwrap();
+    let mut content_type = String::new();
+    if get_path.contains("html") {
+        content_type = "text/html".to_string();
+    }
+    else {
+        content_type = "text/plain".to_string();
+    }
+
+    Response{
+        protocal    : protocal_info.to_string(),
+        status_code : "200".to_string(),
+        server_name : "gan-yixi-web-server/0.1".to_string(),
+        file_type   : content_type,
+        file_length : f.read_to_string(&mut buf).unwrap(),
+        file_content: buf,
+    }
+
+}
+
+
 
 
 // New function to write back with!
